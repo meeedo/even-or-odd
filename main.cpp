4577f315e1be3ff0cc50f37d33754a207e84ba35
meeedo <36135264+meeedo@users.noreply.github.com>
2020-04-19 03:50:33 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*int x;
+    cout<<"enter the number"<<endl;
+    cin>>x;
+    if(x%2==0)
+    {
+        cout<<"even"<<endl;
+
+    }
+    else
+     cout<<"odd"<<endl;
+*/
+
+
+
+
+   /* double y,m;
+    cout<<"enter the radus"<<endl;
+    cin >>y;
+    m=(y*y)*(3.14);
+    cout<<"the area id : "<<m<<endl;
+*/
+
+
+
+
+
+
+
+
+/*
+    string b , v;
+    cout<<"enter the text"<<endl;
+    cin>>b;
+    for (int i=b.size() ; i>=0; i-=1 )
+    {
+        v[i]=b[i];
+
+
+    cout<<v[i]<<" "<<endl;
+    }
+ */
+
+
+
+
+/*
+ int q,w,r;
+ cout<<"enter the first number"<<endl;
+ cin>>q;
+ cout<<"enter the second number"<<endl;
+ cin>>w;
+q=r;
+q=w;
+w=r;
+cout<<"the first number is : "<<q<<endl;
+cout<<"the second number is : "<<w<<endl;
+*/
+
+
+
+
+
+/*
+
+int z=2;
+int x=5;
+int y=4;
+int w=8;
+if (x == y+1 || 7*z != 12-1 && x==w/z)
+{
+    cout<<"yes"<<endl;
+}
+else
+     cout<<"no"<<endl;
+
+*/
+    return 0;
+}
